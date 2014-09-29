5470fbd95922f3b18e2bb9a4f947612cfef0e039
Widny St. Louis <widnystlouis1@gmail.com>
2014-09-29 19:54:18 -0400

    added rating migration to books.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929234408) do
+ActiveRecord::Schema.define(version: 20140929235221) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140929234408) do
     t.string   "amazon_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
 end
