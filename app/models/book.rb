a087473c0f11a4b16e5f13c7ad882c4818670ff1
Widny St. Louis <widnystlouis1@gmail.com>
2014-09-30 08:23:09 -0400

    generated BookGenre model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,14 @@
 class Book < ActiveRecord::Base
+	has_many :book_genres
+	has_many :genres, through: :book_genres
+
 	scope :finished, -> { where.not(finished_on: nil) }
 	scope :recent, -> { where('finished_on > ?', 2.days.ago) }
 	scope :search, -> (keyword){where('keyword LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
 
 	before_save :set_keywords
 	belongs_to :genre
+	
 	def finished?
 		finished_on.present?
 	end
